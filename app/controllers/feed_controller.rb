3981212fc493eaac65896642a4eba6d27df4ec76
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2015-12-10 13:39:24 +0900

    add feeds

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,10 +2,19 @@ require 'feedjira'
 
 class FeedController < ApplicationController
   def index
-    feeds = Feedjira::Feed.fetch_and_parse("http://feeds.feedburner.com/TechCrunch/gaming")
-    p feeds
-    logger.debug(feeds)
-    @entry = feeds.entries
-    #render :nothing => true
+    hatena_it = Feedjira::Feed.fetch_and_parse("http://b.hatena.ne.jp/entrylist/it.rss")
+    @hatena_it_entry = hatena_it.entries.first(5)
+
+    appgiga = Feedjira::Feed.fetch_and_parse("http://plus.appgiga.jp/feed/user")
+    @appgiga_entry = appgiga.entries.first(5)
+
+    codezine = Feedjira::Feed.fetch_and_parse("http://rss.rssad.jp/rss/codezine/new/20/index.xml")
+    @codezine_entry = codezine.entries.first(5)
+
+    google_top = Feedjira::Feed.fetch_and_parse("http://news.google.com/news?hl=ja&ned=us&ie=UTF-8&oe=UTF-8&output=rss&num=5")
+    @google_top_entry = google_top.entries
+
+    google_hcmc = Feedjira::Feed.fetch_and_parse("http://news.google.com/news?ned=us&ie=UTF-8&oe=UTF-8&q=%E3%83%9B%E3%83%BC%E3%83%81%E3%83%9F%E3%83%B3&output=rss&num=5&hl=ja")
+    @google_hcmc_entry = google_hcmc.entries
   end
 end
