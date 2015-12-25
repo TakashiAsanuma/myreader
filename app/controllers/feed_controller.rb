bd978a3eaca2b1464b6db3a0135c321779642c0a
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2015-12-25 18:27:12 +0900

    微修正

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,7 +3,7 @@ require 'feedjira'
 class FeedController < ApplicationController
   def index
     @sections = {}
-    sites = Site.find(:all)
+    sites = Site.all
     sites.each do |site|
       feeds = Feedjira::Feed.fetch_and_parse(site.url)
       @sections[site.name] = feeds.entries.first(5)
