6563b1fa8d4ef83dec8abc7d34122a2785e9d876
Takashi Asanuma <rmacchoj7@gmail.com>
2016-07-09 22:29:18 +0700

    refactor

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,4 @@
 class Feed < ActiveRecord::Base
   belongs_to :site
+  delegate :name, :to => :site, :prefix => true
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -20,5 +20,5 @@
 # Learn more: http://github.com/javan/whenever
 
 every 30.minutes do
-    rake "feed:create"
+    rake "feeds:create"
 end
