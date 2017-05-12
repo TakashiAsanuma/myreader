ab6d645a9f3988251d4da6deaad546de63610a06
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2017-05-12 17:47:21 +0900

    Add subject

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -6,9 +6,11 @@ class FeedController < ApplicationController
     @myreads = Myread.includes(:channel).enabled.user_registered(region, current_user.id)
     if params[:channel_id].present?
       @feeds = Feed.includes(:site).channel_feeds(params[:channel_id])
+      @subject = Channel.find(params[:channel_id]).name
     else
       channels = Channel.top_channels(region)
       @feeds = Feed.top_feeds(channels)
+      @subject = "Top"
     end
   end
 end
